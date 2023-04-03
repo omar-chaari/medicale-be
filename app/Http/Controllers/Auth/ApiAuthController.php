4ea534b2bf7d946734baae968f63733562ddc43c
Omar <3omar.chaari@gmail.com>
2023-04-03 18:03:55 +0100

    update

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
@@ -16,48 +17,56 @@ class ApiAuthController extends Controller
     //
 
     //register profetionel de santé
-    public function register (Request $request) {
-        
+    public function register(Request $request)
+    {
+
         $validator = Validator::make($request->all(), [
             'first_name' => 'required|string|max:255|min:3',
             'last_name' => 'required|string|max:255|min:3',
             'country' => '',
-            'speciality'=> 'required|string|max:255',
-            'phone'=> 'required|string|max:255|min:3',
-            'governorate'=> 'required|string|max:255', 
+            'speciality' => 'required|string|max:255',
+            'phone' => 'required|string|max:255|min:3',
+            'governorate' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            
+
             'address' => 'required|string|max:255|min:3',
-            
+
         ]);
-        if ($validator->fails())
-        {
-            return response(['errors'=>$validator->errors()->all()], 422);
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
         }
-        
-       
-        $request['password']=Hash::make($request['password']);
+
+
+        $request['password'] = Hash::make($request['password']);
         $request['remember_token'] = Str::random(10);
-        $request['verification']=0;
+        $request['verification'] = 0;
 
         $user = User::create($request->toArray());
         $token = $user->createToken('Laravel Password Grant Client')->accessToken;
         $response = ['token' => $token];
+
+
+        if ($request['verification'] == 1) {
+
+            $this->emailVerifProfessional($request);
+        }
         return response($response, 200);
     }
-    
+
 
     public function login(Request $request)
     {
         try {
-            $validateUser = Validator::make($request->all(), 
-            [
-                'email' => 'required|email',
-                'password' => 'required'
-            ]);
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'email' => 'required|email',
+                    'password' => 'required'
+                ]
+            );
 
-            if($validateUser->fails()){
+            if ($validateUser->fails()) {
                 return response()->json([
                     'status' => false,
                     'message' => 'validation error',
@@ -65,7 +74,7 @@ class ApiAuthController extends Controller
                 ], 401);
             }
 
-            if(!Auth::attempt($request->only(['email', 'password']))){
+            if (!Auth::attempt($request->only(['email', 'password']))) {
                 return response()->json([
                     'status' => false,
                     'message' => 'Email & Password does not match with our record.',
@@ -79,7 +88,6 @@ class ApiAuthController extends Controller
                 'message' => 'User Logged In Successfully',
                 'token' => $user->createToken("API TOKEN")->plainTextToken
             ], 200);
-
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => false,
@@ -88,13 +96,25 @@ class ApiAuthController extends Controller
         }
     }
 
-    public function logout (Request $request) {
+    public function logout(Request $request)
+    {
         $token = $request->user()->token();
         $token->revoke();
         $response = ['message' => 'You have been successfully logged out!'];
         return response($response, 200);
     }
-    
 
+    public function emailVerifProfessional($data)
+    {
 
+        $details = [
+            'first_name' => $data->first_name,
+            'last_name' => $data->last_name,
+            'email' => $data->email,
+            'phone' => $data->phone,
+
+        ];
+
+        Mail::to($data->email)->send(new \App\Notifications\Inscription($details));
+    }
 }
---
+++ resources/views/emails/inscription.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bienvenue sur notre plateforme médicale</title>
+</head>
+<body>
+    <h3>Nous somme heureux de vous annoncer que vous avez été ajouté avec succès à notre plateforme médicale en tant que [profession du professionnel de santé]. Vous avez maintenant accès à toutes les fonctionnalités de notre plateforme pour gérer efficacement votre pratique et interagir avec vos patients.
+
+    </h3>
+    <p><b>Nom et Prenom:</b> {{ $details['first_name']  }} {{ $details['last_name']}}</p>
+    <p><b>Adresse mail :</b> {{ $details['email'] }}</p>
+    <p><b>Téléphone Mobile :</b> {{ $details['phone'] }}</p>
+
+
+    <p>Plateforme médicale</p>
+</body>
+</html>
